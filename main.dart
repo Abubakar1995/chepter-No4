060cf895cecf34988e14965dda7852f7300cc455
Abubakar1995 <syedabubakarhussainakhunkhail@gmail.com>
2022-03-14 00:00:28 +0500

    my project

---
+++ main.dart
@@ -0,0 +1,53 @@
+main() {
+//  int age = 21;
+//  String salary ='5000';
+//  double hight =5.6;
+
+//  String sAge = age.toString(); //int to string
+//  print(sAge);
+//  // output 21
+//  double dAge = age.toDouble();// int to double
+//  print(dAge);
+//  // output 21.0
+
+//  int aSalary =int.parse(salary); // string to int 
+//  print(aSalary);
+//  //output 5000
+
+//  double dSalary = double.parse(salary); //string to double
+//  print(dSalary);
+// //output 5000.0
+
+//  int SeHight = hight.toInt(); // double to int
+//  print(SeHight);
+
+//  //output 5
+ 
+//  String dHight = hight.toString(); // double to string
+//  print(dHight);
+//  // output 5,6
+
+String name = 'abubakar hussain';
+print(name.length); // find the number of latters
+// output 8
+print(name.isEmpty);
+// output false
+print(name.isNotEmpty);
+// output true
+print(name.toUpperCase());
+// output ABUBAKAR
+print(name.toLowerCase());
+// output  abubakar 
+print(name.contains('r')); // find word in variable value
+// output true
+print(name.padLeft(10)); // use for space on left side
+  // abubakar
+print(name.trim()); // used for removing space  
+print(name.trimLeft()); // use remove space from left
+print(name.trimRight()); // use remove space from right 
+print(name.split(' ')); // use split do alfaz ko tkseem krna ka leayia
+//output [abubakar, hussain]
+
+
+ }
+  
